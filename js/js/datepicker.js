1413c936fc623ae7e2d22963ea5d679909d250ff
julio-dls <julio.quiroga@davinci.edu.ar>
2019-02-24 16:23:08 -0300

    agregue datepicker en modal y solucione inconveniente

--- js/js/datepicker.js
+++ js/js/datepicker.js
@@ -1,8 +1,12 @@
-$('#datepicker').datepicker({
+$('.datepicker').datepicker({
   weekStart: 1,
   format: 'dd/mm/yyyy',
   daysOfWeekHighlighted: "6,0",
   autoclose: true,
   todayHighlight: true,
 });
-$('#datepicker').datepicker("setDate", new Date());
+$('.datepicker').datepicker("setDate", new Date());
+
+// $(document).ready(function() {
+//   console.log("ready!");
+// });
