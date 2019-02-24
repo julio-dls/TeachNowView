fbae56bb909525536249a97c993af5a2488536d9
julio-dls <julio.quiroga@davinci.edu.ar>
2019-02-24 18:27:55 -0300

    modificacion en cardpf.js elimina alert

--- js/js/cardPF.js
+++ js/js/cardPF.js
@@ -1,5 +1,9 @@
-var $cardPF = $( "<div class='card text-center'> <div class='card-header'> Desarrollador </div> <div class='card-body'> <h5 class='card-title'>Maquetador web, data base mysql, java spring</h5> <p class='card-text'>Desarrollo de diseños web en photoshop y maqueteado con html,css, jquery y javascript. Tabien programa el backend con conexion a base de datos.</p> <a href=''#' class='btn btn-primary'>Eliminar</a> </div> <div class='card-footer text-muted'> 2 days ago </div> </div>" );
+var $cardPF = $("<div class='card text-center'> <div class='card-header'> Desarrollador </div> <div class='card-body'> <h5 class='card-title'>Maquetador web, data base mysql, java spring</h5> <p class='card-text'>Desarrollo de diseños web en photoshop y maqueteado con html,css, jquery y javascript. Tabien programa el backend con conexion a base de datos.</p> <a href='#' class='btn btn-primary btnEliminarCard'>Eliminar</a> </div> <div class='card-footer text-muted'> 2 days ago </div> </div>");
+// var nombre = $('#nombre-agregar').val();
 
-$('.btn2').on('click', function () {
-   $( "#cardperfilprofesional" ).append($cardPF);
- });
+$('.btn2').on('click', function() {
+  $("#cardperfilprofesional").append($cardPF);
+  $('.bd-perfil_profesional-modal-lg').modal('hide');
+  $('#alertPF').remove();
+  // $(".nuevoid").attr('id',"cardperfilprofesional2");
+});
